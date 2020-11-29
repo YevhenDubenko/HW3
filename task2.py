02ce6fd885fad76decf33cbd30707be0e8d36e8c
YevhenDubenko <74934934+YevhenDubenko@users.noreply.github.com>
2020-11-30 00:35:21 +0200

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,6 @@
+phone_number = input ("Enter your phone number for verification:")
+x = len(phone_number)
+if phone_number.isdigit() and x == 10:
+    print("Good boy,thank you ☺ ")
+else :
+    print("Please check the number of entered characters(phone number must be 10 digits)")
