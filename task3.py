2feb6936032d8ac7f1233375898046017d5ae85c
YevhenDubenko <74934934+YevhenDubenko@users.noreply.github.com>
2020-11-29 23:11:31 +0200

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,8 @@
+Other_name = input("Tell your name: ")
+My_name_Correct = Other_name.lower()
+My_name = "yevhen"
+My_name == My_name_Correct
+if My_name == My_name_Correct:
+    print("Good Boy, Yevhen")
+else:
+    print("Your not Ivzhen")
