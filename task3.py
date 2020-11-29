af6c04bc0b024ae7d6d12cb8e87f39596dab88cc
YevhenDubenko <74934934+YevhenDubenko@users.noreply.github.com>
2020-11-30 01:25:07 +0200

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,7 +1,6 @@
 Other_name = input("Tell your name: ")
 My_name_Correct = Other_name.lower()
 My_name = "yevhen"
-My_name == My_name_Correct
 if My_name == My_name_Correct:
     print("Good Boy, Yevhen")
 else:
